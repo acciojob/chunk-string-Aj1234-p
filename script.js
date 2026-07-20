23149e967cc4ed8e4a960db730531f49336e4d84
Ajit Pandey <ajitytsurat0707@gmail.com>
2026-07-20 10:50:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function stringChop(str, size) {
-  // your code here
+  let i =0, n = str.length;
+   let res = [];
+	if(n===0) return res;
+   while(i<n){
+	   let string = "";
+	   if(i+size<=n)
+	    string = str.substr(i,size);
+	   else
+		string = str.substr(i,n);
+	   res.push(string);
+	   i+=size;
+   }
+	return res;
 }
 
 // Do not change the code below
